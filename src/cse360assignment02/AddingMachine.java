1b13ed36bf21c1b7ddd6c0d959eb20c34a9c60a0
Nil <nilmu@asu.edu>
2020-10-07 21:25:07 -0700

    modified addingmachine code

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -3,21 +3,29 @@
 // Lecture Time: Thu 9am - 10:15am
 // Project: assignment1, version control
 
+package cse360assign
 public class AddingMachine {
     private int total;
+    private int add_value;
+    private int sub_value;
 
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
+        add_value = 0;
+        sub_value = 0;
     }
 
     public int getTotal () {
-        return 0;
+        return total;
     }
 
     public void add (int value) {
+        total += value;
+
     }
 
     public void subtract (int value) {
+        total -= value;
     }
 
     public String toString () {
