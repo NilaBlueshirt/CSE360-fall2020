9a742054f1bb4ff78c882e10d05d1d3b59fa2270
Nil <nilmu@asu.edu>
2020-10-07 21:35:22 -0700

    modified version

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,36 +2,48 @@
 // ASU ID: 1215686342
 // Lecture Time: Thu 9am - 10:15am
 // Project: assignment1, version control
+// Github URL: https://github.com/NilaBlueshirt/CSE360-fall2020
 
-package cse360assign
+package cse360assignment02;
+
 public class AddingMachine {
     private int total;
-    private int add_value;
-    private int sub_value;
+    private String history;
 
-    public AddingMachine () {
+    public AddingMachine() {
         total = 0;  // not needed - included for clarity
-        add_value = 0;
-        sub_value = 0;
+        history = "0";
     }
 
-    public int getTotal () {
+    public int getTotal() {
         return total;
     }
 
-    public void add (int value) {
-        total += value;
-
+    public void add(int value) {
+        total = total + value;
+        history = history + " + " + value;
     }
 
-    public void subtract (int value) {
-        total -= value;
+    public void subtract(int value) {
+        total = total - value;
+        history = history + " - " + value;
     }
 
-    public String toString () {
-        return "";
+    public String toString() {
+        return history;
     }
 
     public void clear() {
+        total = 0;
+        history = "0";
+    }
+
+    public static void main(String[] args) {
+        AddingMachine test = new AddingMachine();
+        test.add(4);
+        test.subtract(2);
+        test.add(5);
+        System.out.println("Total: " + test.getTotal());
+        System.out.println(test.toString());
     }
 }
