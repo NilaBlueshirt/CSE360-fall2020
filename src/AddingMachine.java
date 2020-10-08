07a1b75f65ba894388ee184d133e71ac705929b8
Nil <nilmu@asu.edu>
2020-10-07 18:47:48 -0700

    This is the first version of the given code.

---
+++ src/AddingMachine.java
@@ -0,0 +1,29 @@
+// Name: Tianchen Mu
+// ASU ID: 1215686342
+// Lecture Time: Thu 9am - 10:15am
+// Project: assignment1, version control
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+    }
+
+    public void add (int value) {
+    }
+
+    public void subtract (int value) {
+    }
+
+    public String toString () {
+        return "";
+    }
+
+    public void clear() {
+    }
+}
